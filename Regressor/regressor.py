0c6332613e1c691e188ddf9a0981724aa6226ef6
Ali-AliAli-Ali <64184988+Ali-AliAli-Ali@users.noreply.github.com>
2020-12-26 15:38:40 +0300

    Add files via upload

---
+++ Regressor/regressor.py
@@ -0,0 +1,75 @@
+import random
+import math
+from math import exp, log
+import numpy as np
+from numpy import linalg, arange
+import matplotlib.pyplot as plt
+
+
+fig = plt.figure(figsize=(8, 8))
+maxX = 0
+n = 10#random.randint(10, 1000)
+mode = input("Set the mode: lin, pol or exp - ")
+
+
+if mode == 'lin':
+    A = np.ones((n, 2))
+    AT = np.ones((2, n))
+    Y = np.ones((n, 1))
+    for i in range(0, n):
+        x = i
+        maxX = max(maxX, x)
+        y = i*random.randint(20, 50) #random.randint(0, i*20)
+        plt.scatter(x, y)
+        A[i][1] = AT[1][i] = x
+        Y[i][0] = y
+
+    omega = np.dot(np.dot(linalg.inv(np.dot(AT, A)), AT), Y)
+
+    Xes = arange(0, maxX, 0.01)
+    plt.plot(Xes, omega[0][0] + omega[1][0] * Xes)
+
+
+elif mode == 'pol':
+    degree = int(input("Enter the degree then - "))
+    degree += 1
+
+    A = np.ones((n, degree))
+    AT = np.ones((degree, n))
+    Y = np.ones((n, 1))
+    for i in range(0, n):
+        x = i
+        maxX = max(maxX, x)
+        y = 1000*np.arctan(i-500)-i + random.randint(-100, 200)
+        plt.scatter(x, y)
+
+        for j in range(1, degree):
+            A[i][j] = AT[j][i] = pow(x, j)
+        Y[i][0] = y
+
+    omega = np.dot(np.dot(linalg.inv(np.dot(AT, A)), AT), Y)
+
+    Xes = arange(0, maxX, 0.01)
+    sum = 0
+    for i in range(degree): sum += omega[i][0] * pow(Xes, i)
+    plt.plot(Xes, sum)
+
+
+else:
+    A = np.ones((n, 2))
+    AT = np.ones((2, n))
+    Y = np.ones((n, 1))
+    for i in range(0, n):
+        x = i
+        maxX = max(maxX, x)
+        y = -i*i + i*40 + 405 #random.randint(1, (i+1)*20)#exp(i)
+        plt.scatter(x, y)
+        A[i][1] = AT[1][i] = x
+        Y[i][0] = log(y)
+
+    omega = np.dot(np.dot(linalg.inv(np.dot(AT, A)), AT), Y)
+    Xes = arange(0, maxX, 0.01)
+
+    plt.plot(Xes, pow(math.e, omega[0][0] + omega[1][0] * Xes))
+
+plt.show()
